217e433fbe701bd8079bbadce1a42be1898bfdc7
Alice Lin <lalataemin2289@gmail.com>
2025-12-26 23:45:00 +0800

    VO微修改

--- src/main/java/com/showise/order/model/OrderVO.java
+++ src/main/java/com/showise/order/model/OrderVO.java
@@ -3,10 +3,15 @@ package com.showise.order.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.showise.member.model.MemberVO;
+import com.showise.orderfood.model.OrderFoodVO;
+import com.showise.orderticket.model.OrderTicketVO;
 import com.showise.session.model.SessionVO;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -15,11 +20,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "order_info")
 public class OrderVO implements Serializable {
+	private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,8 +47,14 @@ public class OrderVO implements Serializable {
     @Column(name = "order_status", nullable = false)
     private Integer orderStatus;
 
-    @Column(name = "order_create_time", updatable = false)
+    @Column(name = "order_create_time", updatable = false, insertable=false)
     private Timestamp orderCreateTime;
+    
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval=true)
+    private Set<OrderFoodVO> orderFoods = new HashSet<>();
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval=true)
+    private Set<OrderTicketVO> orderTickets = new HashSet<>();
 
 	public Integer getOrderId() {
 		return orderId;
@@ -90,4 +103,22 @@ public class OrderVO implements Serializable {
 	public void setOrderCreateTime(Timestamp orderCreateTime) {
 		this.orderCreateTime = orderCreateTime;
 	}
+
+	public Set<OrderFoodVO> getOrderFoods() {
+		return orderFoods;
+	}
+
+	public void setOrderFoods(Set<OrderFoodVO> orderFoods) {
+		this.orderFoods = orderFoods;
+	}
+
+	public Set<OrderTicketVO> getOrderTickets() {
+		return orderTickets;
+	}
+
+	public void setOrderTickets(Set<OrderTicketVO> orderTickets) {
+		this.orderTickets = orderTickets;
+	}
+
+	
 }
