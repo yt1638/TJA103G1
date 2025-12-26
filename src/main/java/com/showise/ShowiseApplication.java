80e4b32bc8eb477d47df77b9fb9c34fe91ef5116
yutong <wytong1638@gmail.com>
2025-12-26 14:30:52 +0800

    First commit

---
+++ src/main/java/com/showise/ShowiseApplication.java
@@ -0,0 +1,13 @@
+package com.showise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/showise/ShowiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
