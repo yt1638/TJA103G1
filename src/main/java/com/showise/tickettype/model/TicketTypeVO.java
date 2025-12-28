b5c4252e1a40e9103122043412dda78ca04b80d3
Ben Liu <ben19930115@gmail.com>
2025-12-28 20:43:29 +0800

    TicketVO

--- src/main/java/com/showise/tickettype/model/TicketTypeVO.java
+++ src/main/java/com/showise/tickettype/model/TicketTypeVO.java
@@ -1,24 +1,19 @@
 package com.showise.tickettype.model;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
 
-import org.springframework.format.annotation.DateTimeFormat;
+import com.showise.orderticket.model.OrderTicketVO;
 
-import com.showise.member.model.MemberVO;
-import com.showise.movie.model.MovieVO;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
@@ -37,7 +32,7 @@ public class TicketTypeVO implements Serializable {
 
     @Column(name = "ticket_PRICE", nullable = false)
     @NotNull(message = "價格: 請勿空白")
-    private Integer ticketPRICE;
+    private Integer ticketPrice;
 
     @NotNull(message = "內容: 請勿空白")
     @Column(name = "ticket_DESCRIPTION", nullable = false)    
@@ -45,13 +40,10 @@ public class TicketTypeVO implements Serializable {
     
     @NotNull(message = "圖片: 請勿空白")
     @Column(name = "ticket_IMAGES", nullable = false)    
-    private Short ticketImages;
-
-
-    public TicketTypeVO getticketTypeId(TicketTypeVO ticketTypeId) {
-        return ticketTypeId;
-
-}
+    private byte[] ticketImages;
+    
+    @OneToMany(mappedBy = "ticketType", cascade = CascadeType.ALL)
+    private Set<OrderTicketVO> orderTickets = new HashSet<>();
 
 	public Integer getTicketTypeId() {
 		return ticketTypeId;
@@ -69,12 +61,12 @@ public class TicketTypeVO implements Serializable {
 		this.ticketName = ticketName;
 	}
 
-	public Integer getTicketPRICE() {
-		return ticketPRICE;
+	public Integer getTicketPrice() {
+		return ticketPrice;
 	}
 
-	public void setTicketPRICE(Integer ticketPRICE) {
-		this.ticketPRICE = ticketPRICE;
+	public void setTicketPrice(Integer ticketPrice) {
+		this.ticketPrice = ticketPrice;
 	}
 
 	public String getTicketDescription() {
@@ -85,13 +77,20 @@ public class TicketTypeVO implements Serializable {
 		this.ticketDescription = ticketDescription;
 	}
 
-	public Short getTicketImages() {
+	public byte[] getTicketImages() {
 		return ticketImages;
 	}
 
-	public void setTicketImages(Short ticketImages) {
+	public void setTicketImages(byte[] ticketImages) {
 		this.ticketImages = ticketImages;
 	}
 
+	public Set<OrderTicketVO> getOrderTickets() {
+		return orderTickets;
+	}
+
+	public void setOrderTickets(Set<OrderTicketVO> orderTickets) {
+		this.orderTickets = orderTickets;
+	}
 	
 }
