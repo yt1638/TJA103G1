b850dc96a2dcfd791d0d90e4436ded840092a14a
Alice Lin <lalataemin2289@gmail.com>
2026-01-03 10:58:14 +0800

    OrderRepository新增

--- src/main/java/com/showise/order/model/OrderRepository.java
+++ src/main/java/com/showise/order/model/OrderRepository.java
@@ -6,6 +6,8 @@ public interface OrderRepository extends JpaRepository<OrderVO, Integer>{
 	
 	 boolean existsBySession_SessionIdAndOrderStatus(Integer sessionId,Integer orderStatus);
 	 
+	 boolean existsByOrder_Session_SessionIdAndSeatId(Integer sessionId, Integer seatId);
+	 
 //	 List<OrderVO> findByMemberId(Integer memberId);
 
 }
